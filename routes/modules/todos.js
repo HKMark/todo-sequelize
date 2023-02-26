b10ed00fb3029393311f6285a563c101f70c05ff
Mark Law <hongkongmark@gmail.com>
2023-02-26 23:11:18 +0800

    feat: add create todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,10 +4,23 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 router.get('/todos/:id', (req, res) => {
   const UserId = req.user.id
   const id = req.params.id
-  
+
   return Todo.findOne({
     where: { id, UserId }
   })
